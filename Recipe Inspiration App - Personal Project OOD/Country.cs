be1d70e9ec2cada98245f777a1b9d0e97a1c5c25
rudgerylopes94 <63984559+rudgerylopes94@users.noreply.github.com>
2022-02-12 16:02:07 +0000

    Added objects, new class

---
+++ Recipe Inspiration App - Personal Project OOD/Country.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Inspiration_App___Personal_Project_OOD
+{
+    public class Country : IComparable
+    {
+
+        //Properties
+        public string CountryName { get; set; }
+
+
+        public string  TraditionalFood { get; set; }
+
+
+        //Constructors
+        public Country(string countryname, string tradFood)
+        {
+            CountryName = countryname;
+            TraditionalFood = tradFood;
+        }
+
+        //default constructor
+        public Country()
+        {
+
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return CountryName;
+        }
+
+        public int CompareTo(object obj)
+        {
+
+            return CountryName.CompareTo(obj);
+        }
+    }
+}
--- Recipe Inspiration App - Personal Project OOD/MainWindow.xaml.cs
+++ Recipe Inspiration App - Personal Project OOD/MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace Recipe_Inspiration_App___Personal_Project_OOD
     /// </summary>
     public partial class MainWindow : Window
     {
+        //List os countries
+        List<Country> allCountries = new List<Country>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void cbboxCountries_Loaded(object sender, RoutedEventArgs e)
+        {
+            //declaring objects
+            Country c1 = new Country() { CountryName = "Mexico", TraditionalFood = "Chilaquiles" };
+            Country c2 = new Country() { CountryName = "Brazil", TraditionalFood = "Feijoada" };
+            Country c3 = new Country() { CountryName = "France", TraditionalFood = "Ratatouille" };
+            Country c4 = new Country() { CountryName = "China", TraditionalFood = "Kung Pao Chicken" };
+            Country c5 = new Country() { CountryName = "South Africa", TraditionalFood = "Bobotie" };
+            Country c6 = new Country() { CountryName = "Japan", TraditionalFood = "Okonomiyyaki" };
+
+            //adding them to th collection
+            allCountries.Add(c1);
+            allCountries.Add(c2);
+            allCountries.Add(c3);
+            allCountries.Add(c4);
+            allCountries.Add(c5);
+            allCountries.Add(c6);
+
+            //Displaying countries
+            cbboxCountries.ItemsSource = allCountries;
+
+
+
         }
     }
 }
